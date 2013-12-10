b0e50d761ba7d33bfad092455f0e8760329180ac
game03 <game03@game03ui-Mac-Pro.local>
2013-12-10 16:42:26 +0900

    merge

--- tower defense/GameLayer.cpp
+++ tower defense/GameLayer.cpp
@@ -1,8 +1,9 @@
 #include "GameLayer.h"
 #include "SimpleAudioEngine.h"
 #include "GameSprite.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "Map.h"
+#include "Tower.h"
+#include "Mob.h"
 
 using namespace cocos2d;
 using namespace CocosDenshion;
@@ -10,7 +11,6 @@ using namespace CocosDenshion;
 GameLayer::~GameLayer() {
     //release CCArray
 }
-
 
 CCScene* GameLayer::scene()
 {
@@ -53,6 +53,23 @@ bool GameLayer::init()
     
     this->schedule(schedule_selector(GameLayer::update));
 	_lastPosTouch = CCPointZero;
+    
+    Map *terrain = new Map();
+    
+    terrain->NewTower(BASIC_L1, 0, 0);
+    for (int i = 0; i < 18; i++) {
+        for (int n = 0; n < 14; n++) {
+            if (terrain->_map[n][i] == NULL)
+                std::cout << 0 << " ";
+            else
+                std::cout << 1 << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+    // faire un CCArray de monstre
+    // faire un CCArray de tower avec pour size 104
+    
     return true;
 }
 
@@ -109,6 +126,23 @@ void GameLayer::update (float dt) {
     if (_began == false) {
         return;
     }
+    
+    // boucle de jeu (update dans cocos2D) a adapter pou que ca tinne dans le update, basically all while will be if
+    int level = 0;
+    
+    // when in update
+    while (level < 30 /* && life > 0*/) { // modify to a if in cocos and call endGame() and replace with gamelayer variable
+        for (int i = 0; i < 15; i++) //  nombre arbitraire a adapter, creation de niveau
+            MobFactory((mobType)level); //  stocker le retour dans le CCArray, mettre un delai entre chaque creation de mob., should be moved to next wave
+        // add mob array empty condition empty to continue something like :
+        // while (mobArray.lenght != 0)
+        {
+            // boucle de jeu
+        }
+        level++; // call nextWave in gameLayer
+        std::cout << "level " << level << " finished" << std::endl;
+    }
+    endGame();
 }
 
 void GameLayer::winLife() {
